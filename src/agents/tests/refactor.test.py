a206f874ba7c1c81ef7268168b7b207edb72f181
Vishvam10 <vishvam.sundararajan@contentstack.com>
2024-11-04 17:13:56 +0530

    fix: (backend) remove delimiting logic from developer and refactor agent and add JSON schema instead

--- src/agents/tests/refactor.test.py
+++ src/agents/tests/refactor.test.py
@@ -1,16 +1,10 @@
 from agents.refactor import RefactorAgent
 
-from constants import SRC_DIR
-from utils.file import join_paths
-
-APP_TXT_PATH = join_paths([SRC_DIR, "agents", "tests", "app.txt"])
 
 def main():
-
-    refactor_agent = RefactorAgent(app_txt_path=APP_TXT_PATH)
+    refactor_agent = RefactorAgent()
     refactor_agent.run()
 
 
 if __name__ == "__main__":
-    
     main()
